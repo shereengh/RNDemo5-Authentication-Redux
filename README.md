f74d69bc4e8ec9c1b20223e07e0bd4aab612a2df
Hussein Almutawa <hussein@joincoded.com>
2019-10-05 18:27:37 +0300

    starting point updated

---
+++ README.md
@@ -0,0 +1,9 @@
+ # RNDemo5 - Authentication
+
+1. Walk through the code:
+
+   - Show them the `LoginForm` and the state.
+
+#### Basic Auth
+
+2. 
--- components/LoginForm.js
+++ components/LoginForm.js
@@ -9,22 +9,39 @@ import {
   Button,
   Text
 } from "native-base";
+
+import { connect } from "react-redux";
+
+// Actions
+// import { login } from "./redux/actions";
+
 class LoginForm extends Component {
   state = {
     username: "",
     password: ""
   };
+  handleChange = event => {
+    this.setState(event);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.login(this.state);
+  };
+
   render() {
+    const { username, password } = this.state;
+    console.log(this.state);
     return (
       <Container>
         <Header />
         <Content>
           <Form>
             <Item>
-              <Input placeholder="Username" />
+              <Input name="username" value={username} placeholder="Username" />
             </Item>
             <Item last>
-              <Input placeholder="Password" />
+              <Input value={password} placeholder="Password" name="password" />
             </Item>
             <Button>
               <Text>Login</Text>
